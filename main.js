120a6d0876266a94f989acd2cc1983b8010c2d23
Wedsney Alves Domingos <wedsney22@gmail.com>
2023-03-12 09:40:56 -0300

    circulos animados,coloridos e responsivos ao mouse

--- main.js
+++ main.js
@@ -24,36 +24,75 @@ var c = canvas.getContext('2d')
 //c.arc(x ,y ,30 ,0, Math.PI * 2, false);
     //c.stroke();
    // c.strokeStyle ="purple"}
+   
+var mouse = {
+    x: undefined,
+     y: undefined
+}
 
+var maxRadius = 40;
+var minRadius = 2;
+var colorArray = [
+    "antiqueWhite",
+    "lightPink",
+    "lightPurple",
+    "lightBlue",
+    "rgb(114, 207, 207)"
+]
 
-    function Circle(x, y, dx, dy, radius){
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.radius = radius;
+window.addEventListener('mousemove', 
+    function(event) {
+        mouse.x = event.x ;
+        mouse.y = event.y ;
+    
+})
 
-        this.draw = function()  {
-            c.beginPath();
-            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            c.strokeStyle ="lightblue";
-            c.stroke();
-            c.fillStyle = "rgba(30, 155, 210    , 0.5)"
-            c.fill();
-        }
-        this.update = function (){
-            if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-                this.dx = - this.dx;
-            }
-            if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-                this.dy = - this.dy;
-            }
-        this.x += this.dx;
-        this.y += this.dy
+window.addEventListener("resize", function(){
+    canvas.width = this.window.innerWidth;
+    canvas.height = this.window.innerHeight;
 
-        this.draw();
-        }
+    init();
+})
+
+function Circle(x, y, dx, dy, radius){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.minRadius = radius;
+    this.radius = radius;
+    this.color =  colorArray[Math.floor(Math.random()* colorArray.length)];
+
+    this.draw = function()  {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
     }
+    this.update = function (){
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = - this.dx;
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = - this.dy;
+        }
+    this.x += this.dx;
+    this.y += this.dy
+
+    //interatividade
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+        if(this.radius < maxRadius){
+            this.radius += 1;
+        }
+       
+    }
+   else if(this.radius > this.minRadius){
+    this.radius -= 1;
+   }
+
+    this.draw();
+    }
+}
 
    //x= Math.random()* innerWidth;
    //var dx = (Math.random()- 0.5)*20;
@@ -63,17 +102,20 @@ var c = canvas.getContext('2d')
 
    var circleArray = [];
 
-   for(var i=0; i < 100; i++){
-        var radius = 30
-        x= Math.random()* (innerWidth - radius*2)+ radius;
-        var dx = (Math.random()- 0.5)*2;
-        y=Math.random()*(innerHeight - radius*2)+ radius;
-        var dy = (Math.random()- 0.5)*2;
-        circleArray.push(new Circle(x, y, dx, dy, radius    ));
+    function init(){
+        circleArray = [];
 
-   }
-
-    
+        for(var i=0; i < 1000; i++){
+             var radius = Math.random() * 3 + 1;
+             x= Math.random()* (innerWidth - radius*2)+ radius;
+             var dx = (Math.random()- 0.5)*2;
+             y=Math.random()*(innerHeight - radius*2)+ radius;
+             var dy = (Math.random()- 0.5)*2;
+             
+             circleArray.push(new Circle(x, y, dx, dy, radius    ));
+     
+        }
+    }  
 
   
     function animate(){
@@ -85,3 +127,4 @@ var c = canvas.getContext('2d')
 
     }   
    requestAnimationFrame(animate);
+   init();
